8c9b1ec2e91597b261323a3ec22ff3dab468e3cb
Tobilence <tobiasfurtlehner@hotmail.com>
2023-07-19 12:50:10 +0200

    fixed typos

--- data/Data.ts
+++ data/Data.ts
@@ -1,7 +1,7 @@
 export const strings = {
 	about: {
 		title: "About Me",
-		text: "In 2020 I graduated from the higher technical college HTL Spengergasse in the computer science department and have been working as a software developer ever since. Simultaneously, I study Psychology and Data Science at university. I am fascinated by the impact technology can have on our lives, how we interact with technology and how we can use technology to become both happier and healthier. This is exactly why I love the combination of my degrees. Research in psychology gives us valuable information about ourselves. We can then use this information to create smart artificial systems that help us in our day to day lives."
+		text: "In 2020 I graduated from the higher technical college HTL Spengergasse in the computer science department and have been working as a software developer ever since. Simultaneously, I study Psychology and Data Science at the university of Vienna and the university of applied sciences St. Pölten.. I am fascinated by the impact technology can have on our lives, how we interact with technology and how we can use technology to become both happier and healthier. This is exactly why I love the combination of my degrees. Research in psychology gives us valuable information about ourselves. We can then use this information to create smart artificial systems that help us in our day to day lives."
 	},
 	interests: [
 		{
@@ -23,17 +23,17 @@ export const strings = {
 	skills: [
 		{
 			title: "Data Science",
-			text: "I love creating software. However, recently I have become more and more interested in data. I believe that data can improve almost any aspect of business and life in general. I currently study Data Science and Business Analytics at the university of applied science St. Pölten.",
+			text: "I love creating software. However, recently I have become more and more interested in data. I believe that data can improve almost any aspect of business and life in general. I currently study Data Science and Business Analytics at the university of applied sciences St. Pölten.",
 			imgSrc: "/images/system_design.png"
 		},
 		{
 			title: "Fullstack Software Development",
-			text: "I have been working as a fullstack developer for 3 years. The projects I have work on include websites such as mobil365.at and hausbesuche.com as well as a mobile calorie tracking and cooking app. I developed server side features as well as client side web and mobile apps.",
+			text: "I have been working as a fullstack developer for 3 years. The projects I have worked on include websites such as mobil365.at and hausbesuche.com as well as a mobile calorie tracking and cooking app. I developed server side features as well as client side web and mobile apps.",
 			imgSrc: "/images/simple_track_1.png"
 		},
 		{
 			title: "iOS Development | Data Analysis",
-			text: "As part of my diploma project, I developed an IOT solution that manages plant boxes. Users received advice on keeping their plants happy and healthy based on real time data that was sent from sensors in their plant box. Furthermore, users could view comprehensive history analytics of their plants’ well being as well as tips on improving the environment to match each plant’s specific needs.",
+			text: "As part of my diploma project, I have developed an IOT solution that manages plant boxes. In the app, users receive advice on keeping their plants happy and healthy based on real time data that was sent from sensors in their plant box. Furthermore, users can view comprehensive history analytics of their plants’ well being as well as tips on improving the environment to match each plant’s specific needs.",
 			imgSrc: "/images/plant_care.png"
 		}
 	]
